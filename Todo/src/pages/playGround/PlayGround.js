53be082fa8aebf59599fa0ef11e6e8ce6111b238
rysenko <orysenko@blhs.org>
2018-07-04 18:44:35 +0300

    added computer logic

--- Todo/src/pages/playGround/PlayGround.js
+++ Todo/src/pages/playGround/PlayGround.js
@@ -6,53 +6,162 @@ import {TaskList} from "../TaskList/TaskList";
 import './playGround.scss';
 
 
-
 export class PlayGround extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       squares: Array(9).fill(null),
+      isComputer: false,
     };
   }
 
-  addSign = () => {
-    this.setState({
-      value: 'X',
-    })
-  };
+  /* const squares = Object.assign({}, this.state.squares); */
+
+/*  handleField = (event) => {
+    const target = event.target;
+    this.setState({ [target.name]: { value: target.value } });
+    console.log(target.text);
+  }; */
+
+  /*   this.setState({
+       squares: squares,
+       isComputer: !this.state.isComputer,
+     }); */
 
   onClick = (event) => {
-    const { target } = event;
-    console.log(target.value);
-    this.setState({ [target.name]: target.value });
+    const {squares, isComputer} = this.state,
+        playerSign = "X";
+
+/*    console.log(isComputer); */
+
+    if (isComputer) return;
+    squares[event.target.id] = playerSign;
+    this.setSquares(squares);
+    this.computer();
   };
 
+  computer = () => {
+
+/*  const fl = (field) => this.setState(Object.assign({}, field)); */
+
+    const { squares } = this.state,
+        length = squares.length,
+        middle = (length - 1) / 2,
+        compSign = "O";
+
+
+/*    if (!this.state.isComputer) return; */
+
+/*    for (let i = 0; i < length; i++) {
+
+      console.log(squares[i]);
+    }*/
+
+    this.calculateWinner(squares);
+
+
+/*    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        console.log(squares[a]);
+        return squares[a];
+      }
+    }*/
+
+
+    if (!squares[middle]) {
+      squares[middle] = compSign;
+      this.setSquares(squares)
+    }
+
+  };
+
+
+
+  calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+/*
+    for (let i = 0; i < lines.length; i++) {
+      console.log(lines[i]);
+      console.log(squares);
+
+    switch (lines[i]) {
+
+      case [0, 1, 2]:
+
+        console.log("0 - line");
+        break;
+
+      case [0, 3, 6]:
+        console.log("3 - line");
+        break;
+    }
+
+    }
+*/
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+
+      if (squares[a] && squares[a] === squares[b] ) {
+        console.log(squares[a]);
+        return squares[a];
+      }
+    }
+
+  };
+
+
+  setSquares = (squares) => {
+    this.setState({
+      squares: squares,
+      isComputer: this.state.isComputer ? false : this.state.isComputer,
+    });
+  };
 
   componentDidMount() {
 
   }
 
+
   render() {
 
-    const field = new Array(9).fill(null);
-/*    console.log(field);*/
+    const { squares } = this.state;
 
-    const { user, gameCounter } = this.props;
+/*    const { user, gameCounter } = this.props;*/
 
     return (
         <div className="playGround">
-
           <ul className="ground"> {
-
-            this.state.squares.map((box, boxIndex) => (
+            squares.map((box, boxIndex) => (
                 <li
                     key={boxIndex}
                     id={boxIndex}
                     className="cell"
-                    onClick={(target) => this.onClick(target)}
+                    onClick={this.onClick}
                 >
-                  {this.state.value}
+                  {box}
                 </li>
             ))}
           </ul>
