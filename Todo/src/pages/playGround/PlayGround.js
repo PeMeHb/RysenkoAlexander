1b6afaa2e6bf1d8d41ef3b9b66329873a79b6443
rysenko <orysenko@blhs.org>
2018-08-29 17:44:18 +0300

    comp iq

--- Todo/src/pages/playGround/PlayGround.js
+++ Todo/src/pages/playGround/PlayGround.js
@@ -31,7 +31,7 @@ export class PlayGround extends Component {
 
   onClick = (event) => {
     const {squares, isComputer} = this.state,
-      playerSign = "X";
+        playerSign = "X";
 
     console.log(isComputer);
 
@@ -52,43 +52,39 @@ export class PlayGround extends Component {
 
   };
 
-  randomCorner = () => {
+    randomCorner = (corners) => {
 
-    const squares = this.state.squares.slice(),
-      corners = [0, 2, 6, 8];
+      const squares = this.state.squares.slice();
+/*        corners = [0, 2, 6, 8];*/
 
-    let randomCorner = Math.floor(Math.random() * corners.length);
+      console.log(corners);
 
-    console.log(squares[corners[randomCorner]]);
+      let randomCorner = Math.floor(Math.random() * corners.length);
 
-    squares[corners[randomCorner]] ? this.randomCorner() : squares[corners[randomCorner]] = "O";
+      console.log(squares[corners[randomCorner]]);
 
-    this.setSquares(squares);
+      squares[corners[randomCorner]] ? this.randomCorner() : squares[corners[randomCorner]] = "O";
 
-    /*    if (!squares[corners[randomCorner]]) {
+      this.setSquares(squares);
 
-        }
-        squares[corners[randomCorner]] = "O";*/
-
-  };
+    };
 
 
   computerIQ = () => {
     const squares = this.state.squares.slice(),
-      length = squares.length,
-      middle = (length - 1) / 2,
-      compSign = "O",
-      lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-      ];
-
+        length = squares.length,
+        middle = (length - 1) / 2,
+        compSign = "O",
+        lines = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+        ];
 
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
@@ -99,7 +95,13 @@ export class PlayGround extends Component {
       //    console.log("a = b" + squares[a] === squares[b] + '\n' + "a = c" + squares[a] === squares[c]);
       //
       //  }
-      if (squares[a] && squares[a] === squares[b] && !squares[c]) {
+
+      if (squares[a] === "O" && squares[b] === "O" && !squares[c]) squares[c] = "O";
+      if (squares[a] === "O" && squares[c] === "O" && !squares[b]) squares[b] = "O";
+      if (squares[b] === "O" && squares[c] === "O" && !squares[a]) squares[a] = "O";
+
+
+        if (squares[a] && squares[a] === squares[b] && !squares[c]) {
         console.log("1", squares);
 
         squares[c] = "O";
@@ -129,20 +131,34 @@ export class PlayGround extends Component {
       return;
     }
 
-    if (!squares[0] || !squares[2] || !squares[6] || !squares[8]) {
-      const corners = [0, 2, 6, 8];
-      let randomCorner = Math.floor(Math.random() * corners.length);
 
-      console.log(squares[corners[randomCorner]]);
 
-      squares[corners[randomCorner]] ? this.randomCorner() : squares[corners[randomCorner]] = "O";
+      if (!squares[0] || !squares[2] || !squares[6] || !squares[8]) {
+        const corners = [0, 2, 6, 8];
+        this.randomCorner(corners);
+        return;
+      }
+      if (!squares[1] || !squares[3] || !squares[5] || !squares[7]) {
+        const corners = [1, 3, 5, 7];
+        this.randomCorner(corners);
 
-      this.setSquares(squares);
+      }
 
-    }
+/*
+    let randomCorner = Math.floor(Math.random() * corners.length);
+
+    console.log(squares[corners[randomCorner]]);
+
+    squares[corners[randomCorner]] = "O";
+
+    //   squares[corners[randomCorner]] ? this.randomCorner() : squares[corners[randomCorner]] = "O";
+
+    this.setSquares(squares);*/
+
 
 
   };
+
 
   playAgain = () => {
     this.setState({
@@ -170,22 +186,22 @@ export class PlayGround extends Component {
     /*    const { user, gameCounter } = this.props;*/
 
     return (
-      <div className="playGround">
-        <ul className="ground"> {
-          squares.map((box, boxIndex) => (
-            <li
-              key={boxIndex}
-              id={boxIndex}
-              className="cell"
-              onClick={this.onClick}
-            >
-              {box}
-            </li>
-          ))}
-        </ul>
+        <div className="playGround">
+          <ul className="ground"> {
+            squares.map((box, boxIndex) => (
+                <li
+                    key={boxIndex}
+                    id={boxIndex}
+                    className="cell"
+                    onClick={this.onClick}
+                >
+                  {box}
+                </li>
+            ))}
+          </ul>
 
-        <button className="butt" onClick={this.playAgain}>Play again</button>
-      </div>
+          <button className="butt" onClick={this.playAgain}>Play again</button>
+        </div>
 
 
     );
